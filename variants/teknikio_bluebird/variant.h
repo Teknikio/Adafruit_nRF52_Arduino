7e4ec62692c064d2ef7ada2c3074d7ee5bf074de
Fabien <depraetref@gmail.com>
2020-03-16 18:48:51 -0400

    Add some test for bluebird
    Add test for each function of the bluebird board, and modification of the variant definition

--- variants/teknikio_bluebird/variant.h
+++ variants/teknikio_bluebird/variant.h
@@ -22,8 +22,8 @@
 /** Master clock frequency */
 #define VARIANT_MCK       (64000000ul)
 
-//#define USE_LFXO      // Board uses 32khz crystal for LF
-#define USE_LFRC    // Board uses RC for LF
+#define USE_LFXO      // Board uses 32khz crystal for LF
+//#define USE_LFRC    // Board uses RC for LF
 
 /*----------------------------------------------------------------------------
 *        Headers
@@ -37,27 +37,29 @@ extern "C"
 #endif // __cplusplus
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (18)
-#define NUM_DIGITAL_PINS     (12)
-#define NUM_ANALOG_INPUTS    (6)
+#define PINS_COUNT           (48)
+#define NUM_DIGITAL_PINS     (48)
+#define NUM_ANALOG_INPUTS    (8)
 #define NUM_ANALOG_OUTPUTS   (0)
 
 // LEDs
 #define PIN_NEOPIXEL         (47)
 
+#define LED_BUILTIN          (25)
+#define LED_STATE_ON         1         // State when LED is litted
+
 /*
 * Analog pins
 */
-#define PIN_A0               (29)  // not really analog!
-#define PIN_A1               (31)
-#define PIN_A2               (2)
-#define PIN_A3               (3)
-#define PIN_A4               (4)
-#define PIN_A5               (5)
-#define PIN_A6               (6)
-#define PIN_A7               (7) // not really analog!
-#define PIN_A8               (8)
-#define PIN_A9               (11)
+#define PIN_A0               (2)
+#define PIN_A1               (3)
+#define PIN_A2               (4)
+#define PIN_A3               (5)
+#define PIN_A4               (28)
+#define PIN_A5               (29)
+#define PIN_A6               (30)
+#define PIN_A7               (31)
+
 
 static const uint8_t A0  = PIN_A0 ;
 static const uint8_t A1  = PIN_A1 ;
@@ -67,8 +69,7 @@ static const uint8_t A4  = PIN_A4 ;
 static const uint8_t A5  = PIN_A5 ;
 static const uint8_t A6  = PIN_A6 ;
 static const uint8_t A7  = PIN_A7 ;
-static const uint8_t A8  = PIN_A8 ;
-static const uint8_t A9  = PIN_A9 ;
+
 #define ADC_RESOLUTION    14
 
 /*
@@ -78,15 +79,18 @@ static const uint8_t A9  = PIN_A9 ;
 #define PIN_SERIAL_RX       (7)
 #define PIN_SERIAL_TX       (8)
 
-
-
 /*
 * Wire Interfaces
 */
-#define WIRE_INTERFACES_COUNT 2
+#define WIRE_INTERFACES_COUNT 1
 
 #define PIN_WIRE_SDA        (44)
 #define PIN_WIRE_SCL        (43)
+#define PIN_WIRE_INT		(45)
+
+#define PIN_BUZZER      	(46)
+
+#define PIN_COLOR_ENABLE    (30)
 
 #define USB_MSC_BLOCK_SIZE    512
 #define USB_MSC_BLOCK_COUNT   ((2*1024*1024) / USB_MSC_BLOCK_SIZE)
